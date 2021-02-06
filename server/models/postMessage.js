598f8cc3a4906e41efd28d4dbdf10c65420444b3
John Clayton <jcjr805@gmail.com>
2021-02-06 14:26:23 -0800

    "Fix for postSchema error"

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -18,6 +18,6 @@ const postSchema = mongoose.Schema({
 
 });
 
-const PostMessage = mongoose.model('PostMessage, postSchema');
+const PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
